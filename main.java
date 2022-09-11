b5807e0ede213ceaa5657a121931373da2379360
Bruno Mossmann <41453787+brunomossmann007@users.noreply.github.com>
2022-09-11 18:21:01 -0300

    Random
    versao1.2

--- main.java
+++ main.java
@@ -1,11 +1,40 @@
 import java.util.Scanner;
 
 public class main {
-     
-	 public static void main(String[] args) {
-        double doubleRandomNumber = Math.random() * 1000;
-        System.out.println("doubleRandomNumber = " + doubleRandomNumber);
-        int randomNumber = (int)doubleRandomNumber;
-        System.out.println("randomNumber = " + randomNumber);
-    }
+
+    static void congruenteLinear(int Xo, int m, int a, int c, int[] randomNums, int noOfRandomNums){
+
+    randomNums[0] = Xo;
+    for(int i = 1; i < noOfRandomNums; i++){
+
+    randomNums[i] = ((randomNums[i - 1] * a) + c) % m;
 }
+}
+
+
+public static void main(String[] args){
+
+int Xo = 0;
+int a = 18;
+int c = 1;
+int m = 5;
+
+
+int noOfRandomNums = 1000;
+
+
+int[] randomNums = new int[noOfRandomNums];
+
+
+congruenteLinear(Xo, m, a, c,
+randomNums,
+noOfRandomNums);
+
+
+for(int i = 0; i < noOfRandomNums; i++)
+{
+System.out.print(randomNums[i] + " ");
+}
+}
+}
+
